7b74126fa2e9f3a720b3cbb8a68e15621256990f
jambonbill <jambonbill@gmail.com>
2016-10-05 18:06:34 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Profitability of Industrial Sectors</title>
+
+<link rel="shortcut icon" href="img/favicon.jpg">
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/jquery-ui.css" rel="stylesheet">
+<link href="./css/d3.css" rel="stylesheet">
+<link href="./css/main.css" rel="stylesheet">
+
+<!-- Font awesome -->
+<link href="./css/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+<script src="./js/jquery.min.js"></script>
+<script src="./js/bootstrap.js"></script>
+<script src="./js/d3.v3.min.js"></script>
+<script src="./js/main.js"></script>
+
+</head>
+
+<body>
+
+<div class='container bs-docs-container'>
+
+	<h1><img src='img/logo_ffi_48px.png' width='40'> Profitability of Industrial Sectors</h1>
+
+	<?php 
+	include "main.php";
+	?> 
+
+</div>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,9 @@
+
+<div class="alert alert-info fade in" role="alert">
+<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+This is a little description to get started  (in case you need one)
+</div>
+
+
+
+
