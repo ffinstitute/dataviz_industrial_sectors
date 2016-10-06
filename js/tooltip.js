b588f2ce93b044e33986e4e1d9ed2d9e7f98a417
jambonbill <jambonbill@gmail.com>
2016-10-06 18:34:28 +0800

    tooltip

---
+++ js/tooltip.js
@@ -0,0 +1,26 @@
+var ttdiv = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 1e-6);
+
+function ttover(html){
+    if(html)ttdiv.html(html);
+    //console.log('ttover');
+    ttdiv.transition().duration(100).style("opacity", 1);
+}
+
+function ttmove(){
+  ttdiv.style("left", ttleft ).style("top", tttop);
+}
+
+function ttleft(){
+  var max = $("body").width()-$("div.tooltip").width() - 20;
+    return  Math.min( max , d3.event.pageX + 10 ) + "px";
+}
+
+function tttop(){
+	var max = $("body").height()-$("div.tooltip").height() - 20;
+    return  Math.min( max , d3.event.pageY + 10 ) + "px";	
+}
+
+function ttout(){
+    //console.log('ttout');
+    ttdiv.transition().duration(200).style("opacity", 1e-6);
+}
